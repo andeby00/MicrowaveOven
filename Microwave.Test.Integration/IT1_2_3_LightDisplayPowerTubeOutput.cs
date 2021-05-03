605072af8e3043538ef4a85ee3ddbc0dd5e2cb60
aspschou <57791851+aspschou@users.noreply.github.com>
2021-05-03 20:59:23 +0200

    Added first test

--- Microwave.Test.Integration/IT1_2_3_LightDisplayPowerTubeOutput.cs
+++ Microwave.Test.Integration/IT1_2_3_LightDisplayPowerTubeOutput.cs
@@ -1,19 +1,39 @@
-﻿using NUnit.Framework;
+﻿using Microwave.Classes.Boundary;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.IO;
 
 namespace Microwave.Test.Integration
 {
     [TestFixture]
-    class IT1_2_3_LightDisplayPowerTubeOutput
+    public class IT1_2_3_LightDisplayPowerTubeOutput
     {
+        private IOutput _output;
+        private Light _light;
+        private Display _display;
+        private PowerTube _powerTube;
+
         [SetUp]
         public void Setup()
         {
+            _output = Substitute.For<IOutput>();
+            _light = new Light(_output);
+            _display = new Display(_output);
+            _powerTube = new PowerTube(_output);
         }
 
         [Test]
-        public void Test1()
+        public void LightOn_ToOutputTest()
         {
-            Assert.Pass();
+            // Arrange
+            _light.TurnOn();
+
+            // Act
+
+            // Assert
+            _output.Received().OutputLine("Light is turned on");
         }
     }
 }

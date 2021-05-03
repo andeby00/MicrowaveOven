9d81d155a305d368d3ea377cf5b79be9dcd0ebbe
aspschou <57791851+aspschou@users.noreply.github.com>
2021-05-03 20:30:27 +0200

    Readded files

---
+++ Microwave.Test.Integration/IT1_2_3_LightDisplayPowerTubeOutput.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    class IT1_2_3_LightDisplayPowerTubeOutput
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Microwave.Test.Integration/IT4_CookControllerDisplayPowerTube.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IT4_CookControllerDisplayPowerTube
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

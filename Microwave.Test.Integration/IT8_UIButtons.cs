f9fc3dbbf224a9186de2e33ff27aa2a31f9248ac
aspschou <57791851+aspschou@users.noreply.github.com>
2021-05-05 09:17:28 +0200

    Updated IT8

--- Microwave.Test.Integration/IT8_UIButtons.cs
+++ Microwave.Test.Integration/IT8_UIButtons.cs
@@ -10,24 +10,32 @@ namespace Microwave.Test.Integration
     [TestFixture]
     public class IT8_UIButtons
     {
+        private IDoor _door;
         private IOutput _output;
-        private IUserInterface _ui;
-        private Light _light;
-        private Display _display;
-        private PowerTube _powerTube;
+        private Button _powerButton;
+        private Button _timeButton;
+        private Button _startCancelButton;
+        private ITimer _timer;
+        private ILight _light;
+        private IDisplay _display;
+        private IPowerTube _powerTube;
         private ICookController _cooker;
-        private Button _button;
-       
+        private IUserInterface _ui;
+
         [SetUp]
         public void Setup()
         {
+            _door = Substitute.For<IDoor>();
             _output = Substitute.For<IOutput>();
-            _ui = Substitute.For<IUserInterface>();
-            _cooker = Substitute.For<ICookController>();
+            _powerButton = new Button();
+            _timeButton = new Button();
+            _startCancelButton = new Button();
+            _timer = new Timer();
             _light = new Light(_output);
             _display = new Display(_output);
             _powerTube = new PowerTube(_output);
-            _button = new Button();
+            _cooker = new CookController(_timer, _display, _powerTube);
+            _ui = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cooker);
         }
 
         [Test]
